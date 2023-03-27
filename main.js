ece4b7273d2df30b78f901247a2b5db15680212e
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-03-27 22:50:30 +0500

    b/m game

---
+++ main.js
@@ -0,0 +1,36 @@
+const game = document.querySelector(".game");
+const gamezones = document.querySelectorAll(".gamezone");
+
+
+function rand() {
+    for (let i = 0; i < 5; i++) {
+        let random = Math.floor(Math.random() * gamezones.length);
+        gamezones[random].classList.add('active');
+        gamezones[random].classList.add('selected');
+
+        setTimeout(() => {
+            gamezones[random].classList.remove('active');
+        }, 2000);
+    }
+}
+
+function play() {
+    gamezones.forEach(function(game){
+        game.addEventListener("click", function () {
+            if(game.classList.contains('selected')){
+                game.classList.add("active");
+                game.classList.add("win");
+            }
+            else {
+                alert("Siz Xato qildingiz Xotirangiz judayam past!")
+                location.reload()
+            }
+        })
+    });
+}
+
+function init() {
+    rand()
+    play();
+}
+init();
